d21f69d5911513428234e010ea3b2456a35ad59c
Andrew Ippoliti <acippolit@gmail.com>
2022-10-04 00:15:27 -0400

    Added views I forgot to commit yesterday

---
+++ src/views/UserSignInEmail.vue
@@ -0,0 +1,13 @@
+<template>
+  <user-sign-in-email-vue></user-sign-in-email-vue>
+</template>
+
+<script>
+  import UserSignInEmailVue from '@/components/UserSignInEmail.vue';
+  export default {
+    components:{UserSignInEmailVue},
+    mounted(){
+      document.title = this.$i18n.t('pageTitle.login');
+    }
+  }
+</script>
